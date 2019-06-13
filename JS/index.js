173ea6534fbaba34321ace81f8bd8df34df378c5
tomas <perezgantomas@gmail.com>
2019-06-13 12:47:46 -0400

    NavBar hide and show logic working

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,12 @@
 "use strict";
-{
-  angular
-    .module("App", ["ngRoute", "ngAnimate"]);
-}
+var index = angular.module("App", ["ngRoute", "ngAnimate"]);
+
+index.controller("controller",function($scope){
+   $scope.NavBarDisplay = "hide";
+    $scope.NavBar_Open = function(){
+        if ($scope.NavBarDisplay === "hide")
+            $scope.NavBarDisplay = "NavBar_Container";
+         else
+            $scope.NavBarDisplay = "hide";
+    };
+});
