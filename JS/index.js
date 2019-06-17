5856d7635fa4f080922e0e57314c3b14eacb9c7d
tomas <perezgantomas@gmail.com>
2019-06-16 21:27:28 -0400

    meu amination working(ng-enter)

--- JS/index.js
+++ JS/index.js
@@ -1,31 +1,14 @@
 "use strict";
-var index = angular.module("App", ["ngRoute", "ngAnimate"]);
-index.controller("ngrepeatCtrl", function($scope) {
-
-$scope.NavBarItemList = [
-  {Link: "#!/home", name: 'Home'},
-  {Link: "#!/contact", name: 'Contact'},
-  {Link: "#!/projects", name: 'Projects'},
-  {Link: "#!/mentions", name: 'Mentions'},
-  {Link: "#!/whoarewe", name: 'Who are we?'}
-];
-});
+var index = angular.module("App",  ["ngRoute", "ngAnimate"]);
 
 index.controller("controller",function($scope){
-   $scope.NavBarDisplay = "hide";
-    $scope.NavBar_Open = function(){
-        if ($scope.NavBarDisplay === "hide")
-            $scope.NavBarDisplay = "NavBar";
 
-         else
-            $scope.NavBarDisplay = "hide";
+  $scope.NavBarItemList = [
+    {Link: "#!/home", name: 'Home'},
+    {Link: "#!/contact", name: 'Contact'},
+    {Link: "#!/projects", name: 'Projects'},
+    {Link: "#!/mentions", name: 'Mentions'},
+    {Link: "#!/whoarewe", name: 'Who are we?'}
+  ];
 
-    };
-    $scope.HideNavBar = function(){
-      if ($scope.NavBarDisplay === "hide")
-          $scope.NavBarDisplay = "NavBar";
-
-       else
-          $scope.NavBarDisplay = "hide";
-    }
 });
