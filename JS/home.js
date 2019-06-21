d3b0b34a6366d9561bf8eafc6349df68c2e7c5d5
tomas <perezgantomas@gmail.com>
2019-06-20 23:13:54 -0400

    Setting homeJS binding

--- JS/home.js
+++ JS/home.js
@@ -1,11 +1,12 @@
 "use strict";
-{
-  let home = {
-    templateUrl: "home.html",
 
-    
-  };
-  angular
-   .module("App")
-   .component("home", home);
-}
+angular.
+  module('App').
+  component('home', {
+    templateUrl: "home.html",
+    controller: function controller2() {
+      this.Images = [
+        {CarImg: "Images/Hero_1.jpg", alter: "This is image 1"}
+      ];
+    }
+  });
