6f8c00a7170c3744d44dfc12dbd62fa9300f5777
tomas <perezgantomas@gmail.com>
2019-07-03 12:49:54 -0400

    Projects array entering animation fixex

--- JS/projects.js
+++ JS/projects.js
@@ -3,20 +3,21 @@ angular.
   module('homeApp').
   component('projects', {
     templateUrl: "projects.html",
-    controller: function ProjectController($http){
+    controller: function ProjectController($timeout, $http){
       var self = this;
+      $timeout(function(){
+          $http({
+            method: 'GET',
+            url: 'JSON/HomeProjects.json'
+          }).then(function(response) {
+              self.projectItems = [];
+              self.projectItems = response.data;
+              console.log(self.projectItems);
+              console.log("This is working");
 
-      $http({
-        method: 'GET',
-        url: 'JSON/HomeProjects.json'
-      }).then(function(response) {
-          self.projectItems = response.data;
-          console.log(self.projectItems);
-          console.log("This is working");
-
-        },function (error){
-          console.log("There is an error");
-  });
-
+            },function (error){
+              console.log("There is an error");
+            });
+      },500);
     }
   });
