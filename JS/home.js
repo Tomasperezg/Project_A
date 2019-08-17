2a96b3782204b260a49699c90023b396764a2cd6
TomasP <perezgantomas@gmail.com>
2019-08-17 15:57:50 -0400

    Major Updates to UI-Home Page

--- JS/home.js
+++ JS/home.js
@@ -7,10 +7,13 @@ angular.
     controller: function controller2() {
       var self = this;
       self.Images = [
-        {CarImg: "Images/Hero_1.jpg", alter: "This is image 1", change: "1"}
+        {CarImg: "Images/MainHomeIMG.jpg", alter: "This is image 1", change: "1"}
       ];
       self.LogoI = [
         {LogoImg: "Images/Logo.webp", text: "This is the company logo"}
+      ];
+      self.HomeProIMG = [
+        {ProIMG: "Images/Hero_2.jpg", desc: "This is an example of one project"}
       ];
 
     }
--- JS/index.js
+++ JS/index.js
@@ -11,27 +11,27 @@ index.controller("controller",function($scope){
     {Link: "#!/about", name: 'About'}
   ];
 
-   $scope.NavBarDisplay = "hide";
-    $scope.NavBar_Open = function(){
-        if ($scope.NavBarDisplay === "hide")
-            $scope.NavBarDisplay = "NavBar";
-
-         else
-            $scope.NavBarDisplay = "hide";
-
-    };
-
-    $scope.HideNavBar = function(){
-      if ($scope.NavBarDisplay === "hide")
-          $scope.NavBarDisplay = "NavBar";
-
-       else
-          $scope.NavBarDisplay = "hide";
-    };
-
-    $scope.HideNavBar = function(){
-      $scope.NavBarDisplay = "hide";
-    }
+   // $scope.NavBarDisplay = "hide";
+   //  $scope.NavBar_Open = function(){
+   //      if ($scope.NavBarDisplay === "hide")
+   //          $scope.NavBarDisplay = "NavBar";
+   //
+   //       else
+   //          $scope.NavBarDisplay = "hide";
+   //
+   //  };
+   //
+   //  $scope.HideNavBar = function(){
+   //    if ($scope.NavBarDisplay === "hide")
+   //        $scope.NavBarDisplay = "NavBar";
+   //
+   //     else
+   //        $scope.NavBarDisplay = "hide";
+   //  };
+   //
+   //  $scope.HideNavBar = function(){
+   //    $scope.NavBarDisplay = "hide";
+   //  }
 
 
 });
