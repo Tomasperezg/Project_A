d5f17fc36384396cdfbc7f1a27f3f10356e8ef1a
TomasP <perezgantomas@gmail.com>
2019-09-11 12:54:59 -0400

    commetns slider working

--- JS/about.js
+++ JS/about.js
@@ -5,7 +5,7 @@ angular.
     templateUrl: "about.html",
     controller: function AboutControl (){
       var self = this;
-
+      self._Index = 0;
       self.AboutHeroBNR = [
         {AboutBNR: "Images/Home3.jpg", alter: "This is the image of home 1 kitchen "}
       ];
@@ -13,5 +13,28 @@ angular.
         {comments: "This is an example of a customer comment", userName: "some name"},
         {comments: "This is an example of a customer comment(2)", userName: "some name(2)"}
       ];
+      console.log(self.aboutCustomerComm.length);
+
+      // if a current image is the same as requested image
+      self.isActive = function (index) {
+          return self._Index === index;
+          console.log(self._Index);
+      };
+      // show prev image
+      self.showPrev = function () {
+          self._Index = (self._Index > 0) ? --self._Index : self.aboutCustomerComm.length - 1;
+          console.log("PREV");
+
+      };
+      // show next image
+      self.showNext = function () {
+          self._Index = (self._Index < self.aboutCustomerComm.length - 1) ? ++self._Index : 0;
+          console.log("Next");
+
+      };
+      // show a certain image
+      self.showPhoto = function (index) {
+          self._Index = index;
+      };
     }
   });
